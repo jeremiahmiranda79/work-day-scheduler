93d33ec72cb3742aa91dc5112f1345ce092fb4eb
Jeremiah Miranda <jeremiahmiranda79@gmail.com>
2023-04-19 23:09:34 -0700

    init commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  var saveButtonElement = $('.saveBtn');
+  var clearButtonElement = $('.clearBtn');
+
+  saveButtonElement.on('click', function() {
+    localStorage.setItem($(this).parent().attr('id'), $(this).siblings().eq(1).val());
+  })
+
+  clearButtonElement.on('click', function() {
+    localStorage.clear();
+    setUserInputValuesInTextarea();
+  })
+
+  Init();
+
+  function Init() {
+    applyPastPresentFutureClassToEachTimeBlock()
+    displayCurrentDate();
+    setUserInputValuesInTextarea();
+  }
+
+  function applyPastPresentFutureClassToEachTimeBlock() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+      var timeDiv = $(this).attr('id');
+
+      if (currentHour == timeDiv) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    })
+  }
+
+  function setUserInputValuesInTextarea() { 
+    $('.description').each(function() {
+      $(this).val(localStorage.getItem($(this).parent().attr('id')));
+    });
+  }
+
+  function displayCurrentDate() {
+    var today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
+    $('#currentTime').text(today.format('h:m:s A'));
+  }
+
+  setInterval(displayCurrentDate, 1000);
+});
